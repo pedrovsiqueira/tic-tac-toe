8705aabaec62c675caaa23241224dbcecfb19924
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-05 22:09:51 -0300

    Add interface props do components

---
+++ src/index.tsx
@@ -0,0 +1,140 @@
+import React, { ReactNode, useState } from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+
+type SquareValue = "X" | "O" | null;
+
+interface SquareProps {
+  onClick(): void;
+  value: SquareValue;
+}
+
+const Square: React.FC<SquareProps> = (props) => {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+};
+
+interface BoardProps {
+  onClick(i: number): void;
+  squares: SquareValue[];
+}
+
+const Board: React.FC<BoardProps> = (props) => {
+  const renderSquare = (i: number): ReactNode => {
+    return <Square value={props.squares[i]} onClick={() => props.onClick(i)} />;
+  };
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
+
+const Game: React.FC = () => {
+  const [history, setHistory] = useState<{squares: SquareValue[]}[]>([{squares: Array(9).fill(null)}])
+  const [stepNumber, setStepNumber] = useState<number>(0)
+  const [xIsNext, setXIsNext] = useState<boolean>(true)
+
+  const handleClick = (i: number): void => {
+    const newHistory = newHistory.slice(0, stepNumber + 1);
+    const current = newHistory[newHistory.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = xIsNext ? "X" : "O";
+    setHistory(newHistory.concat([
+        {
+          squares: squares,
+        },
+      ]))
+
+    setStepNumber(newHistory.length)
+    setXIsNext(!xIsNext)
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
+    });
+  }
+
+  render() {
+    const history = this.state.history;
+    const current = history[this.state.stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
